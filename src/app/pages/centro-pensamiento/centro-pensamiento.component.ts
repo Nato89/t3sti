dbb984bc8c4e081fcdbfa96587c6642b0eefb75d
nrvalenciagc-cpu <nrvalenciagc@gmail.com>
2026-01-21 15:37:56 -0500

    Creación 3 artículos principales

--- src/app/pages/centro-pensamiento/centro-pensamiento.component.ts
+++ src/app/pages/centro-pensamiento/centro-pensamiento.component.ts
@@ -1,6 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
+interface Articulo {
+  id: number;
+  autor: string;
+  fecha: string;
+  fotoAutor: string;
+  portada: string;
+  titulo: string;
+  subtitulo: string;
+  descripcion: string;
+  pdfUrl: string;
+}
+
 @Component({
   selector: 'app-centro-pensamiento',
   imports: [RouterLink],
@@ -11,8 +23,49 @@ export class CentroPensamientoComponent {
   // Estado inicial: lista
   vistaActual: 'lista' | 'red' = 'lista';
 
+  // Mock de artículos
+  articulos: Articulo[] = [
+    {
+    id: 1,
+    autor: 'Virginia Gonfiantini',
+    fecha: '11 de agosto de 2025',
+    fotoAutor: 'assets/images/virginia-gonfiantini.png',
+    portada: 'assets/images/fa-01.png',  // Las imágenes tienen un tamaño de: 741x463,5 px
+    titulo: 'Teoría del derecho modo complejo',
+    subtitulo: 'Un bosquejo sobre la epigenética jurídica', // Cambiar artículo, este no es de ella
+    descripcion: 'La teoría del derecho positivo es inconsistente porque tiende a ser completa...', // Cambiar artículo, este no es de ella
+    pdfUrl: 'assets/pdfs/articulo1.pdf'
+  },
+  {
+    id: 2,
+    autor: 'Carlos Eduardo Maldonado',
+    fecha: '27 de febrero de 2024',
+    fotoAutor: 'assets/images/carlos-maldonado.png',
+    portada: 'assets/images/fa-02.png',  // Las imágenes tienen un tamaño de: 741x463,5 px
+    titulo: 'El problema de la ética: el convivio.',
+    subtitulo: 'Ética e inteligencia artificial',
+    descripcion: 'Este artículo afirma que el problema principal que plantea la IA es...',
+    pdfUrl: 'assets/pdfs/el-problema-de-la-etica-Carlos_Maldonado.pdf'
+  },
+  {
+    id: 3,
+    autor: 'Felipe Rojas Toro',
+    fecha: 'xx de xxxx de 2024',
+    fotoAutor: 'assets/images/felipe-rojas.png',
+    portada: 'assets/images/fa-03.png',    // Las imágenes tienen un tamaño de: 741x463,5 px
+    titulo: 'Desinstitucionalizar la ciencia',
+    subtitulo: 'Revolución científica y políticas públicas',
+    descripcion: 'Este artículo formula un problema: la ciencia en América Latina nace...',
+    pdfUrl: 'assets/pdfs/desinstitucionalizar-la-ciencia-Dr_Felipe_Rojas_Toro.pdf'
+  }
+  ];
+
   cambiarVista(vista: 'lista' | 'red') {
     this.vistaActual = vista;
+  }
+
+  verDetalle(articulo: Articulo) {
+      console.log (`Detalle del artículo:`, articulo);
   }
 
 }
